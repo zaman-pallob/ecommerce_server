d38f417e3a4c19473754dca98a64cf7ca852ffd9
Pallob <atikurzamanpallob@gmail.com>
2023-06-18 14:45:52 +0600

    cart route added

--- config/route_handler.js
+++ config/route_handler.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const api = require('../config/endpoints');
 
 const {getProducts} = require('./../products/product_controller');
+const {addItemToCart,getCartItem,removeCartItem,updateCart} = require('./../cart/cart_controller');
 
 router.get(api.endpoints.products,getProducts);
+router.get(api.endpoints.cart,getCartItem);
+router.delete(api.endpoints.cart,removeCartItem);
+router.post(api.endpoints.cart,addItemToCart);
 
 
 module.exports=router;
